119ba17778a7fcc6cda3f4569a125c3741ccbaa2
John D. Corless <jdcorless@gmail.com>
2015-07-30 07:09:37 -0500

    support directory creation and clean up in shell script

--- git.sh
+++ git.sh
@@ -8,6 +8,17 @@
 # http://blog.rgaiacs.com/_downloads/shell.sh
 # from Rainere Silva
 
+# Make sure directory and file exist
+CWD=$(pwd)
+DIRECTORY=../planets
+FILE=mars.txt
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir $DIRECTORY
+fi
+cd $DIRECTORY
+echo "first line in mars.txt" > $FILE
+git init
+
 # Create the session to be used
 # Target pane 0 will be where visualization is shown
 # Target pane 1 will be where git commands are issued
@@ -15,14 +26,17 @@ tmux new-session -d -s swc
 # Split the window horizontally
 tmux split-window -v
 # Move to directory with file to work on
-tmux send-keys -t 0 "cd planets" enter
+#tmux send-keys -t 0 "cd planets" enter
 tmux send-keys -t 0 "clear" enter
-tmux send-keys -t 1 "cd planets" enter
+#tmux send-keys -t 1 "cd planets" enter
 tmux send-keys -t 1 "clear" enter
 # Run the script in parent directory
-tmux send-keys -t 0 "ipython ../gitdemo3.ipy mars.txt" enter
+tmux send-keys -t 0 "ipython $CWD/git-demo.ipy $DIRECTORY/$FILE" enter
 # Resize the log window to show the last five commands
 # Need to use the number of lines desired + 1
 #tmux resize-pane -t 0 -y 6
 
 tmux attach-session
+# Clean up after exiting tmux session
+cd $CWD
+rm -r $DIRECTORY
